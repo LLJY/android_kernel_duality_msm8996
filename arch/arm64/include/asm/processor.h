28960ef61738bbe2f20f8ec28c8f3fb301f3f770
Slawek <yarpiin@gmail.com>
2017-01-28 14:26:39 +0000

    ARM64: defining macros to optimize power usage
    Author: Sumit Singh
    Here we have defined some macros such as
    cpu_relaxed_read, cpu_relaxed_read_long,
    cpu_read_relax in arch/arm64/include/asm/processor.h
    which will be used to enhance power optimization on Denver.
    
    bug 1440421
    
    Here we are trying to improve power optimazation
    using some macros such
    
    Change-Id: I313e47b35e9a638fe81b7350887f59eeacc45bc1
    Signed-off-by: Sumit Singh <sumsingh@nvidia.com>
    Reviewed-on: http://git-master/r/381950
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sri Krishna Chowdary <schowdary@nvidia.com>
    Reviewed-by: Alexander Van Brunt <avanbrunt@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm64/include/asm/processor.h
+++ arch/arm64/include/asm/processor.h
@@ -181,4 +181,6 @@ static inline void spin_lock_prefetch(const void *x)
 void cpu_enable_pan(void);
 #include <asm-generic/processor.h>
 
+#include <asm-generic/processor.h>
+
 #endif /* __ASM_PROCESSOR_H */
