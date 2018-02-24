d988b3bcc174cbd64e302b34e0b29d797eadb03d
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2018-02-25 03:05:37 +0800

    Revert "crypto: chacha20poly1305 - Skip encryption/decryption for 0-len"
    This reverts commit 56378a1923b6a9567e4470f7510698ff5934c4d6.

--- crypto/chacha20poly1305.c
+++ crypto/chacha20poly1305.c
@@ -122,9 +122,6 @@ static int chacha_decrypt(struct aead_request *req)
 	struct chacha_req *creq = &rctx->u.chacha;
 	int err;
 
-	if (rctx->cryptlen == 0)
-		goto skip;
-
 	chacha_iv(creq->iv, req, 1);
 
 	ablkcipher_request_set_callback(&creq->req, aead_request_flags(req),
@@ -136,7 +133,6 @@ static int chacha_decrypt(struct aead_request *req)
 	if (err)
 		return err;
 
-skip:
 	return poly_verify_tag(req);
 }
 
@@ -388,9 +384,6 @@ static int chacha_encrypt(struct aead_request *req)
 	struct chacha_req *creq = &rctx->u.chacha;
 	int err;
 
-	if (req->cryptlen == 0)
-		goto skip;
-
 	chacha_iv(creq->iv, req, 1);
 
 	ablkcipher_request_set_callback(&creq->req, aead_request_flags(req),
@@ -402,7 +395,6 @@ static int chacha_encrypt(struct aead_request *req)
 	if (err)
 		return err;
 
-skip:
 	return poly_genkey(req);
 }
 
